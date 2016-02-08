6f44cca955ba563c3815f19044090c7f1cfb3051
Aaron Mackall <aaronmckll@gmail.com>
2016-02-08 17:23:50 -0500

    Checkpoint 22 completed

---
+++ address-bloc/models/address_book.rb
@@ -0,0 +1,106 @@
+
+require_relative 'entry.rb'
+
+class AddressBook
+    attr_accessor :entries
+
+    def initialize
+        @entries = []
+    end
+
+    def remove_entry(name, phone, email)
+       delete_entry = nil
+
+       @entries.each do |entry|
+           if name == entry.name && phone == entry.phone_number && email == entry.email
+               delete_entry = entry
+               end
+           end
+
+       @entries.delete(delete_entry)
+
+    end
+
+    def add_entry(name, phone, email)
+
+        index = 0
+        @entries.each do |entry|
+            if name < entry.name
+                break
+            end
+            index += 1
+        end
+        @entries.insert(index, Entry.new(name, phone_number, email))
+    end
+end
+
+require_relative 'entry'
+ require "csv"
+
+ class AddressBook
+   attr_accessor :entries
+
+   def initialize
+     @entries = []
+   end
+
+   def add_entry(name, phone, email)
+     index = 0
+     @entries.each do |entry|
+       if name < entry.name
+         break
+       end
+       index += 1
+     end
+     @entries.insert(index, Entry.new(name, phone, email))
+   end
+
+ # #7
+   def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+ # #8
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+
+     # Implementation goes here
+   end
+ end
+
+ def iterative_search
+   @entries.each do |entry|
+     if entry.name == name
+       return entry
+     end
+ end
+
+   return nil
+ end
+
+
+ def binary_search(name)
+ # #1
+     lower = 0
+     upper = @entries.length - 1
+
+ # #2
+     while lower <= upper
+ # #3
+       mid = (lower + upper) / 2
+       mid_name = @entries[mid].name
+
+ # #4
+       if name == mid_name
+         return @entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+ # #5
+     return nil
+   end
